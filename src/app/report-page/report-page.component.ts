34ad68db9b1aba1e6580833bdbe1b2a000bde3fd
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-03-23 16:15:08 +0600

    new components

---
+++ src/app/report-page/report-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-page',
+  templateUrl: './report-page.component.html',
+  styleUrls: ['./report-page.component.css']
+})
+export class ReportPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
