008fe75e9f6de57011a0b4cc423582650b4d868c
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-03-25 15:56:04 +0600

    main, login, registration pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {RegistrationPageComponent} from './registration-page/registration-page.component';
+import {MainPageComponent} from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'main-page', component: MainPageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'signup', component: RegistrationPageComponent},
+  {path:'', redirectTo:'main-page',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
