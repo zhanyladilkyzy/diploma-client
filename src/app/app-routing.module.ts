ad4f6f128e421099491c593fc79c6f7bbcef2ddd
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-03-31 10:51:02 +0600

    main page fixing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {RegistrationPageComponent} from './registration-page/registration-page.component';
 import {MainPageComponent} from './main-page/main-page.component';
+import {ReportPageComponent} from './report-page/report-page.component';
 
 const routes: Routes = [
   {path:'main-page', component: MainPageComponent},
   {path:'login', component: LoginPageComponent},
   {path:'signup', component: RegistrationPageComponent},
+  {path:'report', component: ReportPageComponent},
   {path:'', redirectTo:'main-page',pathMatch:'full'}
 ];
 
